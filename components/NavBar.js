287adb339fda60795542bd1d4acd4af5b73f664d
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-07-20 14:02:17 -0700

    responsive nav done

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,59 +1,114 @@
 import Link from 'next/link';
 import styled from 'styled-components'; 
+import React, { useState } from "react";
+
 
 const Nav = styled.nav`
-
-height: 80px;
-background: #000;
-color: #fff; 
-display: flex;  
-justify-content: space-between; 
-align-items: center; 
+padding: 0 2rem;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
+background: black;
+top: 0;
+left: 0;
+right: 0;
 `
 
-const StyledLink = styled.a `
-padding: 0rem 2rem; 
-
+const Hamburger = styled.div `
+display: none;
+  flex-direction: column;
+  cursor: pointer;
+  span {
+    height: 2px;
+    width: 25px;
+    background: #7b7fda;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+  }
 `
+const Menu = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+@media (max-width: 768px) {
+  overflow: hidden;
+  flex-direction: column;
+  max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+  transition: max-height 0.3s ease-in;
+  width: 100%;
+}
+`
+
+const MenuLink = styled.a `
+padding: 1rem 2rem;
+cursor: pointer
+text-align: center;
+text-decoration: none;
+color: white;
+transition: all 0.3s ease-in;
+font-size: 0.9rem;
+&:hover {
+  color: #7b7fda;
+}
+`
+
+const Logo = styled.a`
+  padding: 1rem 0;
+  color: white;
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 1.7rem;
+  span {
+    font-weight: 300;
+    font-size: 1.3rem;
+  }
+`;
+
+
+
+
 
 const Navbar = () => {
-    return (
-        <Nav>
-            <div>
-                <Link href = "/" passHref > 
-                <StyledLink>Miguel Ledesma</StyledLink>
-                </Link> 
-            </div>
-
-            <div>
-            <Link href = "/" passHref > 
-                <StyledLink>Home</StyledLink>
-            </Link> 
-
-            <Link href = "/taskResponse" passHref > 
-                <StyledLink>PA Task</StyledLink>
-            </Link> 
-
-
-            <Link href = "/reflection" passHref > 
-                <StyledLink>Reflection</StyledLink>
-            </Link> 
-
-            <Link href = "/email" passHref > 
-                <StyledLink>Email Response</StyledLink>
-            </Link> 
-
-            <Link href = "/recommendation" passHref > 
-                <StyledLink>Recommendations</StyledLink>
-            </Link> 
-
-            <Link href = "/contact" passHref > 
-                <StyledLink>Contact</StyledLink>
-                </Link> 
-            </div>
-
-        </Nav>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Nav>
+    
+     <Logo href="">
+        Interview<span>Homework</span>
+      </Logo>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+      <Menu isOpen={isOpen}>
+          <Link href = "/" passHref>
+        <MenuLink href="">Home</MenuLink>
+        </Link> 
+        <Link href = "/taskResponse" passHref >
+        <MenuLink href="">PA Task</MenuLink>
+        </Link> 
+        <Link href = "/reflection" passHref >
+        <MenuLink href="">Reflection</MenuLink>
+        </Link> 
+        <Link href = "/email" passHref >
+        <MenuLink href="">Email Response</MenuLink>
+        </Link> 
+        <Link href = "/recommendation" passHref>
+        <MenuLink href="">Recommendation</MenuLink>
+        </Link> 
+        <Link href = "/contact" passHref>
+        <MenuLink href="">Contact</MenuLink>
+        </Link> 
+        
+      </Menu>
+    </Nav>
+  );
+};
 
 export default Navbar; 
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
 
 
   return (
+    
     <Layout>
+      <NextUIProvider> 
     <Component {...pageProps} />
+    </NextUIProvider>
     </Layout>
+   
   )
 
   
